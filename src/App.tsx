6ab689297e8040de529445aab2a451bf23644d1d
malikmahad37 <mahidmalik35@gmail.com>
2026-03-10 16:37:19 +0500

    Add ScrollToTop and gallery tab pulse animation

--- src/App.tsx
+++ src/App.tsx
@@ -8,20 +8,24 @@ import Supporters from './pages/Supporters';
 import Gallery from './pages/Gallery';
 import Dates from './pages/Dates';
 import Messages from './pages/Messages';
+import ScrollToTop from './components/ScrollToTop';
 
 const App: React.FC = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="story" element={<Story />} />
-        <Route path="challenges" element={<Challenges />} />
-        <Route path="supporters" element={<Supporters />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="dates" element={<Dates />} />
-        <Route path="messages" element={<Messages />} />
-      </Route>
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="story" element={<Story />} />
+          <Route path="challenges" element={<Challenges />} />
+          <Route path="supporters" element={<Supporters />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="dates" element={<Dates />} />
+          <Route path="messages" element={<Messages />} />
+        </Route>
+      </Routes>
+    </>
   );
 };
 
